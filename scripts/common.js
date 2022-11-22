b12b9950e49e229bd661d918a874ae8dbd75ccfb
Craig McKeachie <craigdmckeachie@gmail.com>
2022-11-22 16:00:36 -0500

    new todo page loading

--- scripts/common.js
+++ scripts/common.js
@@ -1,1 +1,31 @@
 const $q = (selector) => document.querySelector(selector);
+
+function loadUsers() {
+  return fetch("http://localhost:8083/api/users/").then((response) =>
+    response.json()
+  );
+}
+
+function fillUsersDropDown(users) {
+  for (const user of users) {
+    const option = document.createElement("option");
+    option.value = user.id;
+    option.innerText = user.name;
+    usersDropDownList.appendChild(option);
+  }
+}
+
+function loadCategories() {
+  return fetch("http://localhost:8083/api/categories/").then((response) =>
+    response.json()
+  );
+}
+
+function fillCategoriesDropDown(categories) {
+  for (const category of categories) {
+    const option = document.createElement("option");
+    option.value = category.id;
+    option.innerText = category.name;
+    categoriesDropDownList.appendChild(option);
+  }
+}
